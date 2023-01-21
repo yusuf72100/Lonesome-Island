831643a190ea957de2e2d332c78b5bfe9b0a0c90
yusuf72100 <yusufulas481@gmail.com>
2023-01-21 23:29:15 +0100

    r

--- src/main.h
+++ src/main.h
@@ -42,7 +42,6 @@ char* newLogName();
 
 char* eventTime();
 
-//méthode pour dessiner plus facilement les rectangles (forme)
 void dessinerRect(SDL_Rect rectangle, SDL_Renderer *renderer);
 
 void dessinerButton(SDL_Texture *texture, SDL_Renderer *renderer, SDL_Rect rectangle, SDL_Window *window, SDL_Surface *surface);
